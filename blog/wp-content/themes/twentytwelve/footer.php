729587e82f1771e137d2447745c07514ea4bda43
ruirayli <495137142@qq.com>
2013-11-10 10:21:03 +0800

    add baidu statis

--- blog/wp-content/themes/twentytwelve/footer.php
+++ blog/wp-content/themes/twentytwelve/footer.php
@@ -21,5 +21,10 @@
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
+<script type="text/javascript">
+	var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+	document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F03672a57546af6c41d2ce7d4c14b3380' type='text/javascript'%3E%3C/script%3E"));
+</script>
+
 </body>
 </html>
--- index.php
+++ index.php
@@ -146,5 +146,9 @@ get_header(); ?>
         })
       }(window.jQuery)
     </script>
+    <script type="text/javascript">
+      var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+      document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F03672a57546af6c41d2ce7d4c14b3380' type='text/javascript'%3E%3C/script%3E"));
+    </script>
   </body>
 </html>
