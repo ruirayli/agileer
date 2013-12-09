abbd86944ceb42bfdc2a03cd98adf5cacfb2519d
ruirayli <495137142@qq.com>
2013-12-09 19:35:38 +0800

    siderbar 优化

--- blog/wp-content/themes/twentytwelve/index.php
+++ blog/wp-content/themes/twentytwelve/index.php
@@ -15,8 +15,7 @@
  */
 
 get_header(); ?>
-
-	<div id="primary" class="site-content">
+	<div id="primary" class="site-content" >
 		<div id="content" role="main">
 		<?php if ( have_posts() ) : ?>
 
